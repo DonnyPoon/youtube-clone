075770d5563ea23833788cb6aa3564b44e31b302
DonnyPoon <msamboune@gmail.com>
2025-05-29 16:12:19 -0500

    adding ffmpeg

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,11 +1,27 @@
 import express from 'express';
+import ffmpeg from 'fluent-ffmpeg';
 
 const app = express();
 const port = 3000;
+app.post('/process-video', (req, res) => {
+    // Get path of input file from req body
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    if (!inputFilePath){
+        res.status(400).send("Bad Request: Missing input file path.");
+    }
+    if (!outputFilePath){
+        res.status(400).send("Bad Request: Missing output file path.");
+    }
+
+    ffmpeg(inputFilePath)
+        .outputOptions()
+        .
 });
+
+
 
 app.listen(port, () => {
     console.log(`video-processing-service running at http://localhost:${port}`);
