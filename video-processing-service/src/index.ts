efa2c2bf8ca3f1d075d1b168212d6dd4e41339c3
DonnyPoon <msamboune@gmail.com>
2025-05-30 17:33:51 -0500

    added general video processing

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -2,13 +2,17 @@ import express from 'express';
 import ffmpeg from 'fluent-ffmpeg';
 
 const app = express();
-const port = 3000;
-app.post('/process-video', (req, res) => {
-    // Get path of input file from req body
+app.use(express.json());
 
+// TODO: Dynamic codec based on input file format
+
+app.post('/process-video', (req, res) => {
+    
+    // Get path of input file from req body
     const inputFilePath = req.body.inputFilePath;
     const outputFilePath = req.body.outputFilePath;
 
+    // Check for file paths being defined
     if (!inputFilePath){
         res.status(400).send("Bad Request: Missing input file path.");
     }
@@ -16,13 +20,22 @@ app.post('/process-video', (req, res) => {
         res.status(400).send("Bad Request: Missing output file path.");
     }
 
+    // Create ffmpeg command
     ffmpeg(inputFilePath)
-        .outputOptions()
-        .
+        .outputOptions('-vf', 'scale=-1:360') // 360p
+        .on('end', function() {
+            console.log('Processing completed successfully');
+            res.status(200).send('Processing completed successfully');
+        })
+        .on('error', function(err: any) {
+            console.log('An error occurred: ' + err.message);
+            res.status(500).send('An error occurred: ' + err.message);
+        })
+        .save(outputFilePath);
+    
 });
 
-
-
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`video-processing-service running at http://localhost:${port}`);
 });
